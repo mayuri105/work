6ec8e8fd1a270a29a3dc28472ca84fed43c75c1a
Mayuri Patel <61903686+mayuri105@users.noreply.github.com>
2026-04-22 11:16:32 -0500

    social media post project
    MVC OOPS Concept developed.

---
+++ MVCPHP/socialwebapp/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost"); // ex. localhost
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "socialwebapp");
+
+// Define URL
+define("ROOT_PATH", "http://localhost/socialwebapp/"); // https://www.yourdomain.com/ or https://www.yourdomain.com/website/
+
+?>
